9f06d355012aea50d59f5194d7d9a5bea8a850e8
munna shah <munnashah8709@gmail.com>
2024-10-16 19:17:22 +0530

    task4 and task5 is done

---
+++ Task4/index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+// 1. Make the 3rd element in the list have a yellow background color
+const fruitsList = document.getElementsByClassName('fruit');
+fruitsList[2].style.backgroundColor = 'yellow'; 
+
+for (let i = 0; i < fruitsList.length; i++) {
+  fruitsList[i].style.fontWeight = 'bold';
+}
---
+++ Task5/index.js
@@ -0,0 +1,7 @@
+// Write your code below:
+const listItems = document.getElementsByTagName('li');
+listItems[4].style.color = 'blue';
+
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontStyle = 'italic';
+}
